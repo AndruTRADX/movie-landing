f2effb0c1054da716a748c9728a1fcb9240449ee
Andrés Esteban Rodríguez Jiménez <andrudeluxe@gmail.com>
2022-07-19 00:44:38 -0500

    ultimo commit ahora si XD

--- src/secret-files.js
+++ src/secret-files.js
@@ -1,6 +1,4 @@
 // crea un archivo secret-files.js con tu API Key
 const APIKey = '460f02ea2056fb6863d1a346c0ab1989';
 
-const tokenAPI = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NjBmMDJlYTIwNTZmYjY4NjNkMWEzNDZjMGFiMTk4OSIsInN1YiI6IjYyZDVkOWJmMmFmYjI1MDA1NDE5NzUwNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.N0curUHBxhvjTGRCsPOGKEiQukhQQYZ3o44jicz0WEw';
-
 export default APIKey
