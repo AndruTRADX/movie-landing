c3f9c8064bbf34bb326acf7f75807ef551efdffc
Andrés Esteban Rodríguez Jiménez <andrudeluxe@gmail.com>
2022-07-18 23:35:47 -0500

    conexión a la APLI concretada y migración a axios

--- src/main.js
+++ src/main.js
@@ -1,24 +1,58 @@
-import APIKey from './secret-files.js'
+import APIKey from './secret-files.js';
+
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  headers: {
+    "Content-Type": "application.json;charset=utf-8"
+  },
+  params: {
+    "api_key": APIKey
+  },
+})
 
 async function getTrendingMoviesPreview() {
-  const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + APIKey);
-  const data = await res.json();
-  console.log(data.results)
+  const {data} = await api('trending/movie/day');
 
   const movies = data.results;
   movies.forEach(movie => {
+    const trendingPreviewMoviesContainer = document.querySelector('.trendingPreview-movieList');
     const movieContainer = document.createElement('div');
-    movieContainer.classList.add('movie-container');
-
     const movieImg = document.createElement('img');
+    
+    movieContainer.classList.add('movie-container');
     movieImg.classList.add('movie-img');
+
     movieImg.setAttribute('alt', movie.title);
     movieImg.setAttribute('src','https://image.tmdb.org/t/p/w300' + movie.poster_path);
 
     movieContainer.appendChild(movieImg);
-    const trendingPreviewMoviesContainer = document.querySelector('.trendingPreview-movieList');
     trendingPreviewMoviesContainer.appendChild(movieContainer)
   });
 }
 
 getTrendingMoviesPreview()
+
+async function getCategoriesPreview() {
+  const {data} = await api('genre/movie/list');
+
+  const categories = data.genres;
+
+  categories.forEach(category => {
+    const categoriesContainer = document.querySelector('.categoriesPreview-list');
+    const categoryContainer = document.createElement('div');
+    const categoryTitle = document.createElement('h3');
+    const categoryTitleText = document.createTextNode(category.name);
+    
+    categoryTitle.classList.add('category-title');
+    categoryContainer.classList.add('category-container');
+
+
+    categoryTitle.setAttribute('id', 'id' + category.id);
+    
+    categoryTitle.appendChild(categoryTitleText);
+    categoryContainer.appendChild(categoryTitle);
+    categoriesContainer.appendChild(categoryContainer);
+  });
+}
+
+getCategoriesPreview();
